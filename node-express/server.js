ce78358b05c634be65519853b90807714d83c821
ForeverS2C <1399612085@qq.com>
2020-02-19 20:59:15 +0800

    静态文件托管

--- node-express/server.js
+++ node-express/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 //设置模板文件的存放路径
 app.set('views','./views');
@@ -13,7 +14,12 @@ app.get('/',function(req,res){
 
     res.render('index.ejs',{
         name: '张三',
-        age: 123
+        age: 123,
+        list: [
+            {name: '吃饭'},
+            {name: '睡觉'},
+            {name: '哈哈'}
+        ]
     })
 });
 
@@ -61,8 +67,24 @@ app.get('/download',function(req,res){
 });
 
 
+//http://localhost:3000/public/style.css
+// app.get('/public/style.css',function(req,res){
+//     res.sendFile(path.resolve(__dirname,'./public/style.css'))
+// });
 
+//http://localhost:3000/public/index.js
+// app.get('/public/index.js',function(req,res){
+//     res.sendFile(path.resolve(__dirname,'./public/index.js'))
+// });
 
+//设置静态文件托管
+app.use(express.static('public'));
+app.get('/public/style.css',function(req,res){
+    res.sendFile(path.resolve(__dirname,'./public/style.css'))
+});
+app.get('/public/index.js',function(req,res){
+    res.sendFile(path.resolve(__dirname,'./public/index.js'))
+});
 
 
 app.listen(3000);
